ee7cb47e7189406f7e3f2cb72186d9e9f378be0b
GangHeok <gpeegpee@gmail.com>
2015-11-04 01:23:50 +0900

    Fixed build script

--- Platform/Android/build_android.sh
+++ Platform/Android/build_android.sh
@@ -36,7 +36,7 @@ echo
 
 POCO=$1
 POCO_VERSION=$2
-ABI=armeabi
+ABI=armeabi-v7a
 
 
 #Execution starts here
@@ -55,17 +55,19 @@ HOME_DIR=${PWD}
 
 #Make libPoco.a
 witeMessage "Preping libPoco.a..."
-for file in {Foundation$DEBUG,Util$DEBUG,XML$DEBUG,Net$DEBUG,NetSSL$DEBUG,Crypto$DEBUG,Data$DEBUG,DataSQLite$DEBUG}
+#for file in {Foundation$DEBUG,Util$DEBUG,XML$DEBUG,Net$DEBUG,NetSSL$DEBUG,Crypto$DEBUG,Data$DEBUG,DataSQLite$DEBUG}
+for file in Foundation$DEBUG Util$DEBUG XML$DEBUG Net$DEBUG NetSSL$DEBUG Crypto$DEBUG Data$DEBUG DataSQLite$DEBUG
 do
 echo "Framework: Decomposing $file..."
 mkdir -p lib/Android/$ABI/${file}
-(cd lib/Android/$ABI/${file}; arm-linux-androideabi-ar -x ../libPoco$file.a );
+(cd lib/Android/$ABI/${file}; arm-linux-androideabi-ar -x ../libPoco${file}.a );
 done
 cd $HOME_DIR;
 echo "Framework: Archiving objects into libPoco${DEBUG}.a"
 (cd lib/Android/$ABI; arm-linux-androideabi-ar crus libPoco${DEBUG}.a Foundation$DEBUG/*.o Util$DEBUG/*.o XML$DEBUG/*.o Net$DEBUG/*.o NetSSL$DEBUG/*.o Crypto$DEBUG/*.o Data$DEBUG/*.o DataSQLite$DEBUG/*.o );
 cd $HOME_DIR;
-for file in {Foundation$DEBUG,Util$DEBUG,XML$DEBUG,Net$DEBUG,NetSSL$DEBUG,Crypto$DEBUG,Data$DEBUG,DataSQLite$DEBUG}
+#for file in {Foundation$DEBUG,Util$DEBUG,XML$DEBUG,Net$DEBUG,NetSSL$DEBUG,Crypto$DEBUG,Data$DEBUG,DataSQLite$DEBUG}
+for file in Foundation$DEBUG Util$DEBUG XML$DEBUG Net$DEBUG NetSSL$DEBUG Crypto$DEBUG Data$DEBUG DataSQLite$DEBUG
 do
 echo "Framework: Cleaning $file..."
 rm -rf lib/Android/$ABI/${file}
@@ -76,7 +78,8 @@ doneSection
 #Copy includes
 witeMessage "Framework: Copying includes..."
 mkdir -p lib/Android/include/Poco
-for i in {Foundation,Util,XML,Net,NetSSL_OpenSSL,Crypto,Data,Data/SQLite}
+#for i in {Foundation, Util,XML,Net,NetSSL_OpenSSL,Crypto,Data,Data/SQLite}
+for i in Foundation Util XML Net NetSSL_OpenSSL Crypto Data Data/SQLite
 do
 cp -r $POCO/$i/include/Poco/*  lib/Android/include/Poco
 done
